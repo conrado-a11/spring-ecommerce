a202f193eab4e859d6d80da92e9e0d1c2f685875
conrado-a11 <conradovalencia198@gmail.com>
2026-06-02 18:45:35 -0500

    creamos el repository y clases de servicio metodo showproductocontrol

---
+++ src/main/java/com/spring_ecommerce_api/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.spring_ecommerce_api.service;
+
+import com.spring_ecommerce_api.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService  {
+    public Producto save(Producto producto);
+    public Optional<Producto> get(Integer id);
+    public void update(Producto producto);
+    public void delete(Integer id);
+
+}
