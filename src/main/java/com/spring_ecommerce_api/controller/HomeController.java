58b72444b386b48fe886ed12d4430d8ecfd78792
conrado-a11 <conradovalencia198@gmail.com>
2026-06-07 20:16:21 -0500

    funcionalida productos para el usuario (metodo home Directory y template home.html

---
+++ src/main/java/com/spring_ecommerce_api/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.spring_ecommerce_api.controller;
+
+
+import com.spring_ecommerce_api.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @Autowired
+    public ProductoService productoService;
+    @GetMapping("")
+    public String home(Model model){
+
+        model.addAttribute("productos", productoService.findAll() );
+        return "/administrador/usuario/home";
+    }
+}
