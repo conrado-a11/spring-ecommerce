740ae2f8938bd6202b0554a4aef09ee87ddf5894
conrado-a11 <conradovalencia198@gmail.com>
2026-06-08 17:47:32 -0500

    mostrar vista update producto al usuario

--- src/main/java/com/spring_ecommerce_api/controller/HomeController.java
+++ src/main/java/com/spring_ecommerce_api/controller/HomeController.java
@@ -2,15 +2,22 @@ package com.spring_ecommerce_api.controller;
 
 
 import com.spring_ecommerce_api.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
+
+
     @Autowired
     private ProductoService productoService;
     @GetMapping("")
@@ -18,5 +25,9 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll() );
         return "/administrador/usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id) {
+        return "/administrador/usuario/productoHome";
     }
 }
