e10a6a4d1bc61b08cee9f8f32ba249c93ace7f54
conrado-a11 <conradovalencia198@gmail.com>
2026-06-09 00:14:10 -0500

    mostrar informacion del producto en la pagina productoHome

--- src/main/java/com/spring_ecommerce_api/controller/HomeController.java
+++ src/main/java/com/spring_ecommerce_api/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.spring_ecommerce_api.controller;
 
 
+import com.spring_ecommerce_api.model.Producto;
 import com.spring_ecommerce_api.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -24,10 +27,17 @@ public class HomeController {
     public String home(Model model){
 
         model.addAttribute("productos", productoService.findAll() );
-        return "/administrador/usuario/home";
+        return "administrador/usuario/home";
     }
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id) {
+    public String productoHome(@PathVariable Integer id, Model model) {
+        log.info("Id producto enviado como parametro {}", id);
+        Producto producto= new Producto();
+        Optional<Producto>productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
+
         return "/administrador/usuario/productoHome";
     }
 }
