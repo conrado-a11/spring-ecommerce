520cf1fdfb32e9c3bb33970fb62de51a4bfef599
conrado-a11 <conradovalencia198@gmail.com>
2026-06-09 01:57:35 -0500

    redireccion al carrito de compras

--- src/main/java/com/spring_ecommerce_api/controller/HomeController.java
+++ src/main/java/com/spring_ecommerce_api/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -39,5 +40,9 @@ public class HomeController {
         model.addAttribute("producto", producto);
 
         return "/administrador/usuario/productoHome";
+    }
+    @PostMapping("/cart")
+    public String addCart(){
+        return "administrador/usuario/carrito";
     }
 }
