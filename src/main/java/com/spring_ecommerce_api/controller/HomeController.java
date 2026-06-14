9179b6e079c7ca513ad66af6915251645ff324a6
conrado-a11 <conradovalencia198@gmail.com>
2026-06-14 01:54:14 -0500

    funcionalidad para buscar un producto

--- src/main/java/com/spring_ecommerce_api/controller/HomeController.java
+++ src/main/java/com/spring_ecommerce_api/controller/HomeController.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -165,5 +166,14 @@ public class HomeController {
 
         return "redirect:/";
     }
+    @PostMapping("/search")
+    public String searchProducto(@RequestParam String nombre, Model model){
+        log.info("Nombre del Producto: {}", nombre);
+        List<Producto> productos=productoService.findAll().stream().filter(p->p.getNombre().contains(nombre)).collect(Collectors.toList());
+        model.addAttribute("productos", productos);
+
+        return "administrador/usuario/home";
+    }
+
 
 }
