85c6549c7627fcda54ecb48ded0535ccfd9b900d
conrado-a11 <conradovalencia198@gmail.com>
2026-06-13 21:31:49 -0500

    metodo para generar nunero de la orden

--- src/main/java/com/spring_ecommerce_api/service/IOrdenService.java
+++ src/main/java/com/spring_ecommerce_api/service/IOrdenService.java
@@ -2,7 +2,10 @@ package com.spring_ecommerce_api.service;
 
 import com.spring_ecommerce_api.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
 
 }
