77b43f7ac4585be50d790bf81c8e96f3ab6a1788
conrado-a11 <conradovalencia198@gmail.com>
2026-06-16 17:18:30 -0500

    funcionalidad para registro usuario

--- src/main/java/com/spring_ecommerce_api/service/UsuarioServiceImpl.java
+++ src/main/java/com/spring_ecommerce_api/service/UsuarioServiceImpl.java
@@ -6,18 +6,20 @@ import com.spring_ecommerce_api.repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
+
 @Service
 public class UsuarioServiceImpl implements IUsuarioService {
-        @Autowired
-        private IUsuarioRepository usuarioRepository;
 
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
 
     @Override
     public Optional<Usuario> findById(Integer id) {
-
-
         return usuarioRepository.findById(id);
+    }
+    @Override
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
     }
 }
